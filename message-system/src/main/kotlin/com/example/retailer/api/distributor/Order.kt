d37ceb7861e558bde2a0ab83a9ca9d3ca30bde69
Rbster <rbster@yandex.ru>
2021-11-18 21:45:45 +0300

    message-system: db ready?

--- message-system/src/main/kotlin/com/example/retailer/api/distributor/Order.kt
+++ message-system/src/main/kotlin/com/example/retailer/api/distributor/Order.kt
@@ -1,5 +1,10 @@
 package com.example.retailer.api.distributor
 
+import javax.persistence.Column
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.OneToMany
+
 /**
  * Описание заказа
  */
@@ -7,20 +12,25 @@ data class Order(
     /**
      * Уникальный идентификатор заказа на стороне ретейлера
      */
+    @Id
+    @GeneratedValue
     val id: String?,
 
     /**
      * Произвольный адрес доставки
      */
+    @Column
     val address: String,
 
     /**
      * Произвольный получатель доставки
      */
+    @Column
     val recipient: String,
 
     /**
      * Список заказанных товаров
      */
+    @OneToMany
     val items: List<Item>
 )
