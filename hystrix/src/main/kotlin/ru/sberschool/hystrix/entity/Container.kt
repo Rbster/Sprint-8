8d83035b6551ab65ffdc6e773964d6d633009a65
Rbster <rbster@yandex.ru>
2021-11-20 01:34:34 +0300

    hystrix: only is-problem

--- hystrix/src/main/kotlin/ru/sberschool/hystrix/entity/Container.kt
+++ hystrix/src/main/kotlin/ru/sberschool/hystrix/entity/Container.kt
@@ -1,5 +1,7 @@
 package ru.sberschool.hystrix.entity
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 //data class Container(
 //    val count: Long,
 //    val next: String,
@@ -10,16 +12,25 @@ package ru.sberschool.hystrix.entity
 data class Element(
     val name: String,
     val url: String
-)
+) {
+    constructor() : this("", "")
+}
 
 data class Ability(
+//    @JsonProperty("")
     val ability: Element,
-    val isHidden: Boolean,
+//    @JsonProperty("is_hidden")
+    val hidden: Boolean,
+//    @JsonProperty("slot")
     val slot: Long
-)
+) {
+    constructor() : this(Element(), true, 0)
+}
 
 data class Pokemon(
     val abilities: List<Ability>,
     val baseExperience: Long,
     val forms: List<Element>
-)
+) {
+    constructor() : this(listOf(), 1, listOf())
+}
--- hystrix/src/test/kotlin/ru/sberschool/hystrix/SlowlyApiTest.kt
+++ hystrix/src/test/kotlin/ru/sberschool/hystrix/SlowlyApiTest.kt
@@ -1,9 +1,11 @@
 package ru.sberschool.hystrix
 
 import feign.Request
+import feign.RequestTemplate
 import feign.httpclient.ApacheHttpClient
 import feign.hystrix.HystrixFeign
 import feign.jackson.JacksonDecoder
+import feign.jackson.JacksonEncoder
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -11,6 +13,8 @@ import org.mockserver.client.server.MockServerClient
 import org.mockserver.integration.ClientAndServer
 import org.mockserver.model.HttpRequest
 import org.mockserver.model.HttpResponse
+import ru.sberschool.hystrix.entity.Ability
+import ru.sberschool.hystrix.entity.Element
 import ru.sberschool.hystrix.entity.Pokemon
 import java.util.concurrent.TimeUnit
 import kotlin.test.assertEquals
@@ -23,6 +27,7 @@ class SlowlyApiTest {
         .options(Request.Options(1, TimeUnit.SECONDS, 1, TimeUnit.SECONDS, true))
         .target(SlowlyApi::class.java, "http://127.0.0.1:18080", FallbackSlowlyApi())
     lateinit var mockServer: ClientAndServer
+
 
     @BeforeEach
     fun setup() {
@@ -53,5 +58,54 @@ class SlowlyApiTest {
             )
         // expect
         assertEquals(Pokemon(listOf(), 0, listOf()), client.getPokemon(0))
+    }
+
+    @Test
+    fun `getSomething() should return actual data`() {
+        // given
+        val responsePokemon = Pokemon(listOf(Ability(
+            Element("kick", "https://pokemon.co/"),
+            false,
+            1)),
+            100,
+            listOf(Element("base", "https://pokemon.co/"))
+            )
+        val pokemonRequest = RequestTemplate()
+//        JacksonEncoder().mapper
+            JacksonEncoder().encode(responsePokemon, Pokemon::class.java, pokemonRequest)
+        MockServerClient("127.0.0.1", 18080)
+            .`when`(
+                // задаем матчер для нашего запроса
+                HttpRequest.request()
+                    .withMethod("GET")
+                    .withPath("/api/v2/pokemon/1")
+            )
+            .respond(
+                // наш запрос попадает на таймаут
+                HttpResponse.response()
+                    .withStatusCode(200)
+                    .withBody(pokemonRequest.body())
+//                    .withDelay(TimeUnit.SECONDS, 30) //
+
+            )
+        // do
+        val receivedPokemon = client.getPokemon(1)
+        println(responsePokemon)
+        println(receivedPokemon)
+
+        // expect
+        assertEquals(responsePokemon.baseExperience, receivedPokemon.baseExperience)
+        assertEquals(responsePokemon.abilities.size, receivedPokemon.abilities.size)
+        for (i in 0 until responsePokemon.abilities.size) {
+            assertEquals(responsePokemon.abilities[i].ability, receivedPokemon.abilities[i].ability)
+            assertEquals(responsePokemon.abilities[i].slot, receivedPokemon.abilities[i].slot)
+            assertEquals(responsePokemon.abilities[i].hidden, receivedPokemon.abilities[i].hidden)
+        }
+        assertEquals(responsePokemon.forms.size, receivedPokemon.forms.size)
+        for (i in 0 until responsePokemon.forms.size) {
+            assertEquals(responsePokemon.forms[i].name, receivedPokemon.forms[i].name)
+            assertEquals(responsePokemon.forms[i].url, receivedPokemon.forms[i].url)
+        }
+
     }
 }
